8ce7a2450fdb8519f421aa912db26c2e0c3e8a96
Houekpetodji <homahugnon@gmail.com>
2019-04-29 02:11:35 +0200

    cleaning

--- R/grangerTest.R
+++ R/grangerTest.R
@@ -1,7 +1,7 @@
 
-args <- commandArgs()
+args <- commandArgs(TRUE)
 aVector = args[1]
-mylags = args[2]
+mylags = as.integer(args[2])
 resultFile = args[3]
 
   sink(file =resultFile , append = FALSE, type = c("output", "message"),
