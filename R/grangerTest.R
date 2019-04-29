e1800ab1f4bf972330678786e5e0976493af9b08
Houekpetodji <homahugnon@gmail.com>
2019-04-29 02:04:39 +0200

    cleaning

---
+++ R/grangerTest.R
@@ -0,0 +1,10 @@
+
+args <- commandArgs()
+aVector = args[1]
+mylags = args[2]
+resultFile = args[3]
+
+  sink(file =resultFile , append = FALSE, type = c("output", "message"),
+       split = FALSE)
+  library(lmtest)
+  grangertest(aVector$evolution, aVector$corretion, order = mylags, data = aVector)
