c5fa393fda10b8de2cfe7c3452df8d59ff5a4eab
Houekpetodji <homahugnon@gmail.com>
2019-04-29 02:03:15 +0200

    cleaning

--- R/grangerTest.R
+++
@@ -1,6 +0,0 @@
-calculateGranger<-function(aNormalizedVector,myOrder,myFile){
-  sink(file =myFile , append = FALSE, type = c("output", "message"),
-       split = FALSE)
-  library(lmtest)
-  grangertest(liq$evolution ~ liq$corretion, order = myOrder, data = liq)
-}
